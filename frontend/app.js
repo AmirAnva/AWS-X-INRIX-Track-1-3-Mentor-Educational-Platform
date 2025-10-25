b5d8051058ae1f4b89113e5111de21b99a4e32c7
Fyrmed75 <167119612+Fyrmed75@users.noreply.github.com>
2025-10-25 16:26:17 -0700

    front

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,7 @@
 const menu=document.querySelector('#mobile-menu');
-const menuLinks = document.querrySelector('.navbar__menu');
+const menuLinks = document.querySelector('.navbar__menu');
 
-MediaElementAudioSourceNode.addEventListener('click', function(){
+menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
